65cb05ae7bf3a5a62ad9468881703968d6452f32
user9 <user9@fb80056d-18e7-c549-84fd-309fea07f500>
2018-03-08 08:50:52 +0000

    orders 17:50

---
+++ javacafeh/src/members/OrdersDAO.java
@@ -0,0 +1,21 @@
+package members;
+
+import common.DAO;
+
+public class OrdersDAO extends DAO {
+
+	public String createOrderHeader() {
+		connect();
+		String orderHeaderNo = null;
+
+		try {
+
+		} catch (Exception e) {
+
+		} finally {
+
+		}
+		return orderHeaderNo;
+		
+	}// end of createHeaderOrder()
+}
