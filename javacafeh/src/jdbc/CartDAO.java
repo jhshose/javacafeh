1bad100954eb10d9109b7a19fdc21eb78764e5d3
user9 <user9@fb80056d-18e7-c549-84fd-309fea07f500>
2018-03-09 05:07:48 +0000

    1111

--- javacafeh/src/jdbc/CartDAO.java
+++ javacafeh/src/jdbc/CartDAO.java
@@ -10,22 +10,23 @@ import common.DAO;
 
 public class CartDAO extends DAO {
 
-	public ArrayList<CartsDO> selectAll(String p_cart_no) {
+	public ArrayList<CartsDO> selectAll(String p_user_no) {
 		connect();
 		ArrayList<CartsDO> clist = new ArrayList<CartsDO>();
 		CartsDO cdo = null;
 		try {
-			String sql = "select cart_no, detail_no, prod_no, sales_price, order_qty from cart_details where cart_no = ? order by prod_no";
+			String sql = "select user_no, detail_no, prod_no, sales_price, order_qty, get_prod_name(prod_no) p_name from cart_details where user_no = ? order by prod_no";
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, p_cart_no);
+			pstmt.setString(1, p_user_no);
 			ResultSet rs = pstmt.executeQuery();
 			while (rs.next()) {
 				cdo = new CartsDO();
-				cdo.setCart_no(rs.getString("cart_no"));
+				cdo.setUser_no(rs.getString("user_no"));
 				cdo.setDetail_no(rs.getString("detail_no"));
 				cdo.setProd_no(rs.getString("prod_no"));
 				cdo.setSales_price(rs.getInt("sales_price"));
 				cdo.setOrder_qty(rs.getInt("order_qty"));
+				cdo.setProd_name(rs.getString("p_name"));
 				clist.add(cdo);
 			}
 		} catch (Exception e) {
@@ -36,35 +37,45 @@ public class CartDAO extends DAO {
 		return clist;
 	}// end of selectAll
 
-	public HashMap<String, Object> insertProc(CartsDO bean) throws Exception {
+	// public HashMap<String, Object> insertProc(CartsDO bean) throws Exception {
+	public boolean insertProc(CartsDO bean) throws Exception {
 		CallableStatement cstmt = null;
 		int nextId = 0;
 		try {
 			connect();
 			conn.setAutoCommit(false); // 트랜잭션 처리
-			cstmt = conn.prepareCall("{call COMMENTS_INS(?,?,?,?,?)}");
-			// cstmt.setString(1, bean.getName());
-			// cstmt.setString(2, bean.getContent());
-			cstmt.setString(3, "1");
-			cstmt.registerOutParameter(4, java.sql.Types.NUMERIC);
-			cstmt.registerOutParameter(5, java.sql.Types.VARCHAR);
+			// 1.p_user_no, 2.p_prod_no, 3.p_qty, 4.p_price, 5.p_ret, 6.p_msg
+			cstmt = conn.prepareCall("{call create_carts(?,?,?,?,?,?)}");
+			cstmt.setString(1, bean.getUser_no());
+			cstmt.setString(2, bean.getProd_no());
+			cstmt.setInt(3, bean.getOrder_qty());
+			cstmt.setInt(4, bean.getSales_price());
+			cstmt.registerOutParameter(5, java.sql.Types.NUMERIC);
+			cstmt.registerOutParameter(6, java.sql.Types.VARCHAR);
 
 			cstmt.executeUpdate();
 			conn.commit(); // 커밋
-			nextId = cstmt.getInt(4);
-			String out_msg = cstmt.getString(5);
-			System.out.println(out_msg);
-			HashMap<String, Object> map = new HashMap<String, Object>();
-			map.put("id", nextId);
+			// nextId = cstmt.getInt(4);
+			// int out_ret = cstmt.getInt(5);
+			// String out_msg = cstmt.getString(6);
+			// System.out.println(out_msg);
+			// HashMap<String, Object> map = new HashMap<String, Object>();
+			// map.put("id", nextId);
 			// map.put("name", bean.getName());
 			// map.put(("content"), bean.getContent());
-			return map;
+			// map.put("user_no", bean.getUser_no());
+			// return map;
+			System.out.println("insertProc");
+			return true;
 		} catch (Throwable e) {
 			try {
 				conn.rollback(); // 롤백
+				return false;
 			} catch (SQLException ex) {
 			}
 			throw new Exception(e.getMessage());
+		} finally {
+			disconnect();
 		}
 	}// end of insertProc
 }
